ffdb1e2f604dd5e33bcbc82fe6b2e71fbdace1c5
Anuja Nisal Yashohara <rmanuja2003@gmail.com>
2024-12-20 00:04:54 +0530

    chore: project setup & home page

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <p className="text-xl text-white">docs</p>
+    </div>
+  );
+}
